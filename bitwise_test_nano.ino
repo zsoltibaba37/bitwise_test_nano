f996251c692d4749a6c4be6e999e5468d0991274
zsoltibaba37 <petozsolt33@gmail.com>
2022-10-12 11:54:00 +0200

    spell check

--- bitwise_test_nano.ino
+++ bitwise_test_nano.ino
@@ -1,16 +1,16 @@
 // -------------- MASK LIST --------------
-constexpr uint8_t mask0{ 0b00000001 };
-constexpr uint8_t mask1{ 0b00000010 };
-constexpr uint8_t mask2{ 0b00000100 };
-constexpr uint8_t mask3{ 0b00001000 };
-constexpr uint8_t mask4{ 0b00010000 };
-constexpr uint8_t mask5{ 0b00100000 };
-constexpr uint8_t mask6{ 0b01000000 };
-constexpr uint8_t mask7{ 0b10000000 };
+constexpr uint8_t mask0{0b00000001};
+constexpr uint8_t mask1{0b00000010};
+constexpr uint8_t mask2{0b00000100};
+constexpr uint8_t mask3{0b00001000};
+constexpr uint8_t mask4{0b00010000};
+constexpr uint8_t mask5{0b00100000};
+constexpr uint8_t mask6{0b01000000};
+constexpr uint8_t mask7{0b10000000};
 
 // -------------- Flag --------------
 //              76543210
-uint8_t flag0{ 0b00101001 };
+uint8_t flag0{0b00101001};
 uint8_t initFlag = flag0;
 
 // Analog pin
@@ -18,33 +18,35 @@ uint8_t initFlag = flag0;
 // int t{};
 
 // 9 pin
-constexpr uint8_t pin9 { 0b00000010 };  // 9 pin the led
-int64_t timing1{ 100000 };              // led blink treshold
+constexpr uint8_t pin9{0b00000010}; // 9 pin the led
+int64_t timing1{100000};            // led blink threshold
 
 // ------------------ CLASS ------------------
 // ------------------ CLASS ------------------
-class LedTimer {
+class LedTimer
+{
 
 public:
   LedTimer(int64_t _teshold, uint8_t _led);
   void change();
 
 private:
-  uint64_t initTime{ micros() };
-  uint64_t treshold;
+  uint64_t initTime{micros()};
+  uint64_t threshold;
   uint8_t led;
   uint8_t mask;
 };
 
-LedTimer::LedTimer(int64_t _treshold, uint8_t _led)
-  : treshold(_treshold),
-    led(_led) {}
-    
+LedTimer::LedTimer(int64_t _threshold, uint8_t _led)
+    : threshold(_threshold),
+      led(_led) {}
 
-void LedTimer::change(){
-  if(micros() - initTime >= treshold){
+void LedTimer::change()
+{
+  if (micros() - initTime >= threshold)
+  {
     initTime = micros();
-    //PORTB ^= mask1;
+    // PORTB ^= mask1;
     PORTB ^= pin9;
   }
 }
@@ -54,12 +56,13 @@ LedTimer LED9(timing1, pin9);
 
 // -------------- SETUP --------------
 // -------------- SETUP --------------
-void setup() {
+void setup()
+{
   //             98
-  //DDRB |= B00000011;  // 9 pin set to output OR
-  DDRB |= pin9;         // 9 pin set to output
+  // DDRB |= B00000011;  // 9 pin set to output OR
+  DDRB |= pin9; // 9 pin set to output
 
-  //pinMode(pot, INPUT);
+  // pinMode(pot, INPUT);
 
   Serial.begin(115200);
   Serial.println("--------------------------------");
@@ -70,10 +73,11 @@ void setup() {
 
 // -------------- LOOP --------------
 // -------------- LOOP --------------
-void loop() {
+void loop()
+{
 
   LED9.change();
-  
+
   // redout flag0
   Serial.println("Initial flag0");
   Serial.println(flag0, BIN);
@@ -106,11 +110,11 @@ void loop() {
   Serial.println("reset flag0 to initFlag state");
   flag0 = initFlag;
   Serial.println("--------------------------------");
-  //delay(3000);
+  // delay(3000);
 
   // this just a port test
   /*
-  t = map(analogRead(pot), 0, 1024, 100, 1500);  
+  t = map(analogRead(pot), 0, 1024, 100, 1500);
   PORTB ^= mask1;
   PORTB |= mask1;  // set 9 to hign
   delay(t);
